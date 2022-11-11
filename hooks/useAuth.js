994ffc1fcfb450bf5442d0729386d39bfcfdd971
1sh1vam <shivam07.mt@gmail.com>
2022-11-11 20:00:10 +0530

    auth hook

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,8 +1,20 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import { GOOGLE_WEB_CLIENT_ID } from '@env';
+
+WebBrowser.maybeCompleteAuthSession();
 
 const AuthContext = createContext();
 
+console.log('clie', GOOGLE_WEB_CLIENT_ID)
+
 export const AuthProvider = ({ children }) => {
+    const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+        {
+          clientId: GOOGLE_WEB_CLIENT_ID,
+        },
+    );
     return (
         <AuthContext.Provider>
             {children}
