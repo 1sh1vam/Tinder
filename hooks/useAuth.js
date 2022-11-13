b19cd032e2150f10e5ff1de98d6bf2c393dcc72b
1sh1vam <shivam07.mt@gmail.com>
2022-11-13 11:16:59 +0530

    signin with google, firebase expo

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,6 +1,7 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
+import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { GOOGLE_WEB_CLIENT_ID } from '@env';
 
 WebBrowser.maybeCompleteAuthSession();
@@ -15,8 +16,21 @@ export const AuthProvider = ({ children }) => {
           clientId: GOOGLE_WEB_CLIENT_ID,
         },
     );
+
+    useEffect(() => {
+      if (response?.type === 'success') {
+        const { id_token } = response.params;
+        const auth = getAuth();
+        const credential = GoogleAuthProvider.credential(id_token);
+        signInWithCredential(auth, credential);
+      }
+    }, [response])
+
+    const signInWithGoogle = () => {
+      promptAsync();
+    }
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ signInWithGoogle, user: null }}>
             {children}
         </AuthContext.Provider>
     )
