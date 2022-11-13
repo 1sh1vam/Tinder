b9cecd024573eb531178396a94c0526eafb4158b
1sh1vam <shivam07.mt@gmail.com>
2022-11-13 11:17:21 +0530

    refactoring the app component

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import './lib/firebase';
-import ChatScreen from './screens/ChatScreen';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-
-const Stack = createNativeStackNavigator();
+import { AuthProvider } from "./hooks/useAuth";
+import Route from "./Route";
 
 export default function App() {
-  const user = false;
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {user ? (
-          <>
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="ChatScreen" component={ChatScreen} />
-          </>
-        ) : (
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <Route />
+    </AuthProvider>
   );
 }
