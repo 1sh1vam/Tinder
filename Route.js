cdffe6b1ac4a8bb7646ee2dc3937f20ae5f403ff
1sh1vam <shivam07.mt@gmail.com>
2022-11-13 11:17:11 +0530

    seperate component for routing

---
+++ Route.js
@@ -0,0 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import useAuth from './hooks/useAuth';
+import './lib/firebase';
+import ChatScreen from './screens/ChatScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function Route() {
+  const { user } = useAuth();
+
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+          {user ? (
+            <>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="ChatScreen" component={ChatScreen} />
+            </>
+          ) : (
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          )}
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
