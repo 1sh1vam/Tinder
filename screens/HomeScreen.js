01855fcd820289b2d230a05c7eec7863d328fffe
1sh1vam <shivam07.mt@gmail.com>
2022-12-04 17:39:47 +0530

    header styles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,25 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Button from '../components/Button';
+import useAuth from '../hooks/useAuth';
 
 const HomeScreen = () => {
+  const { user } = useAuth();
   return (
     <SafeAreaView>
+      {/* Header */}
+      <View className="flex-row items-center justify-between px-5">
+        <Button>
+          <Image className="w-10 h-10 rounded-full" source={{ uri: user.photoURL }} />
+        </Button>
+        <Button>
+          <Image className="w-14 h-14 rounded-full" source={require('../assets/tinder-logo.png')} />
+        </Button>
+        <Button>
+          <Ionicons name="chatbubbles-sharp" size={30} color="#FF5864" />
+        </Button>
+      </View>
       <Text className="text-red-500 mt-10">HomeScreen</Text>
     </SafeAreaView>
   )
