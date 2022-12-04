74469eea89179e8e6d8049692240f9cb7616dd2f
1sh1vam <shivam07.mt@gmail.com>
2022-12-04 15:09:05 +0530

    login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,21 @@
-import { Button, View } from 'react-native'
+import { Button, ImageBackground, Pressable, SafeAreaView, Text } from 'react-native'
 import React from 'react';
 import useAuth from '../hooks/useAuth';
+import { styled } from 'nativewind';
+
+const StyledImageBackground = styled(ImageBackground);
+const StyledPressable = styled(Pressable);
 
 const LoginScreen = () => {
   const { request, signInWithGoogle } = useAuth();
   return (
-    <View>
-      <Button disabled={!request} title="login" onPress={signInWithGoogle} />
-    </View>
+    <StyledImageBackground className="flex-1" source={require('../assets/tinder.png')}>
+    <SafeAreaView className="flex-1 justify-end">
+        <StyledPressable disabled={!request} onPress={signInWithGoogle} className="bg-white mb-[140px] rounded-full px-4 py-3 w-52 mx-auto active:opacity-50 disabled:opacity-30">
+          <Text className="text-center font-semibold">Signin & get swiping</Text>
+        </StyledPressable>
+    </SafeAreaView>
+    </StyledImageBackground>
   )
 }
 
