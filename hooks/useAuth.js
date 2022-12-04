a92ccf12f7e0b7bd10cb8fb24c227c716e4cd372
1sh1vam <shivam07.mt@gmail.com>
2022-12-04 16:46:30 +0530

    auth error catching

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,35 +1,41 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
-import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential } from "firebase/auth";
 import { GOOGLE_WEB_CLIENT_ID } from '@env';
+import { auth } from "../lib/firebase";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
-        {
-          clientId: GOOGLE_WEB_CLIENT_ID,
-        },
-    );
+  const [error, setError] = useState(null);
+
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+      {
+        clientId: GOOGLE_WEB_CLIENT_ID,
+      },
+  );
 
     useEffect(() => {
-      console.log('inside the effects', response);
       if (response?.type === 'success') {
         const { id_token } = response.params;
-        const auth = getAuth();
         const credential = GoogleAuthProvider.credential(id_token);
         signInWithCredential(auth, credential);
       }
     }, [response])
 
-    const signInWithGoogle = () => {
-      promptAsync();
+    const signInWithGoogle = async () => {
+      try {
+        await promptAsync();
+      } catch(err) {
+        setError(err);
+      }
     }
+
     return (
-        <AuthContext.Provider value={{ request, signInWithGoogle, user: null }}>
+        <AuthContext.Provider value={{ error, request, signInWithGoogle, user: auth.currentUser }}>
             {children}
         </AuthContext.Provider>
     )
