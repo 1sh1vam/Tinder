d51dd194335a7ff9555f333290ffb4f7b404720f
1sh1vam <shivam07.mt@gmail.com>
2022-12-12 19:38:38 +0530

    fixing the keyboard type and age restriction

--- screens/Modal.js
+++ screens/Modal.js
@@ -1,5 +1,5 @@
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import useAuth from '../hooks/useAuth';
 
@@ -13,25 +13,57 @@ const Modal = () => {
   return (
     <View className="flex-1 items-center justify-between pb-10">
       <View>
-      <Image className="w-[200px] h-[100px]" source={require('../assets/tinder-logo-full.png')} />
-      <Text className="capitalize font-semibold text-gray-500 text-xl p-2">Welcome! {user.displayName}</Text>
-      <Text className="text-red-400 font-bold text-center p-4">Step 1: The Profile Pic</Text>
-      <TextInput onChangeText={(text) => setImage(text)} className="text-center text-xl pb-2" placeholder="Enter a Profile Pic URL" />
+        <Image
+          className="w-[200px] h-[100px]"
+          source={require('../assets/tinder-logo-full.png')}
+        />
+        <Text className="capitalize font-semibold text-gray-500 text-xl p-2">
+          Welcome! {user.displayName}
+        </Text>
+        <Text className="text-red-400 font-bold text-center p-4">
+          Step 1: The Profile Pic
+        </Text>
+        <TextInput
+          onChangeText={(text) => setImage(text)}
+          className="text-center text-xl pb-2"
+          placeholder="Enter a Profile Pic URL"
+        />
 
-      <Text className="text-red-400 font-bold text-center p-4">Step 2: The Occupation</Text>
-      <TextInput onChangeText={(text) => setJob(text)} className="text-center text-xl pb-2" placeholder="Enter your occupation" />
+        <Text className="text-red-400 font-bold text-center p-4">
+          Step 2: The Occupation
+        </Text>
+        <TextInput
+          onChangeText={(text) => setJob(text)}
+          className="text-center text-xl pb-2"
+          placeholder="Enter your occupation"
+        />
 
-      <Text className="text-red-400 font-bold text-center p-4">Step 3: The Age</Text>
-      <TextInput onChangeText={(text) => setAge(text)} className="text-center text-xl pb-2" placeholder="Enter the age" />
+        <Text className="text-red-400 font-bold text-center p-4">
+          Step 3: The Age
+        </Text>
+        <TextInput
+          onChangeText={(text) => setAge(text)}
+          className="text-center text-xl pb-2"
+          placeholder="Enter the age"
+          maxLength={2}
+          keyboardType="numeric"
+        />
       </View>
 
-      <Button disabled={incompleteForm} className={`rounded-xl w-64 p-3 ${incompleteForm ? 'bg-gray-400' : 'bg-red-400'}`}>
-        <Text className="text-white font-bold text-xl text-center">Update Profile</Text>
+      <Button
+        disabled={incompleteForm}
+        className={`rounded-xl w-64 p-3 ${
+          incompleteForm ? 'bg-gray-400' : 'bg-red-400'
+        }`}
+      >
+        <Text className="text-white font-bold text-xl text-center">
+          Update Profile
+        </Text>
       </Button>
     </View>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
