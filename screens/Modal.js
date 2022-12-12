eaafa33755f230135c916d8e0b9c28f178b392f0
1sh1vam <shivam07.mt@gmail.com>
2022-12-12 19:22:38 +0530

    modal basic component

--- screens/Modal.js
+++ screens/Modal.js
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import useAuth from '../hooks/useAuth';
 
 const Modal = () => {
+  const { user } = useAuth();
   return (
-    <View>
-      <Text>Modal</Text>
+    <View className="flex-1 items-center">
+      <Image className="w-[200px] h-[100px]" source={require('../assets/tinder-logo-full.png')} />
+      <Text className="capitalize font-semibold text-gray-500 text-xl p-2">Welcome! {user.displayName}</Text>
     </View>
   )
 }
