2d29dbc3de9a20b596827aaa69ef7f334cdbc374
1sh1vam <shivam07.mt@gmail.com>
2023-01-09 20:19:24 +0530

    getting the matched user

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useMemo } from 'react'
+import { getMatchedUserInfo } from '../lib/getMatchedUserInfo'
+import useAuth from '../hooks/useAuth'
 
-const ChatRow = () => {
+const ChatRow = ({ matchDetails }) => {
+  const { user } = useAuth();
+  const matchedUser = useMemo(() => getMatchedUserInfo(matchDetails.users, user.uid), []);
+
+  console.log('matched user', matchedUser);
   return (
     <View>
       <Text>ChatRow</Text>
