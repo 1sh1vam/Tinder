6e91d0bd149c6d89ef89c3a4dc11f45d4ad1b42d
1sh1vam <shivam07.mt@gmail.com>
2023-01-09 20:11:08 +0530

    chat row component

---
+++ components/ChatRow.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChatRow = () => {
+  return (
+    <View>
+      <Text>ChatRow</Text>
+    </View>
+  )
+}
+
+export default ChatRow
+
+const styles = StyleSheet.create({})
