5dd207812de13c629cd4461b1f806a5422138686
1sh1vam <shivam07.mt@gmail.com>
2023-01-09 20:28:03 +0530

    chat row card

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -1,20 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React, { useMemo } from 'react'
 import { getMatchedUserInfo } from '../lib/getMatchedUserInfo'
 import useAuth from '../hooks/useAuth'
+import Button from './Button'
 
 const ChatRow = ({ matchDetails }) => {
   const { user } = useAuth();
   const matchedUser = useMemo(() => getMatchedUserInfo(matchDetails.users, user.uid), []);
 
-  console.log('matched user', matchedUser);
   return (
-    <View>
-      <Text>ChatRow</Text>
-    </View>
+    <Button style={styles.cardShadow} className="flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg">
+        <Image
+            className="w-16 h-16 rounded-full mr-4"
+            source={{ uri: matchedUser.photoUrl }}
+        />
+        <View>
+            <Text className="text-lg font-bold">{matchedUser.displayName}</Text>
+            <Text>Say hi!!</Text>
+        </View>
+    </Button>
   )
 }
 
 export default ChatRow
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    cardShadow: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2
+    }
+})
