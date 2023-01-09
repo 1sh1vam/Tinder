73a9dcc54c4f17a6bb65bd72457701963328b24e
1sh1vam <shivam07.mt@gmail.com>
2023-01-09 19:49:47 +0530

    completing the header component

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons, Foundation } from '@expo/vector-icons';
+import Button from './Button';
 
-const Header = () => {
+const Header = ({ title, callEnabled }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Header</Text>
+    <View className="flex-row items-center justify-between p-2">
+      <View className="flex-row items-center">
+        <Button onPress={() => navigation.goBack()} className="p-2">
+          <Ionicons name="chevron-back-outline" size={34} color="#FF5864" />
+        </Button>
+        <Text className="text-2xl font-bold pl-2">{title}</Text>
+      </View>
+      {callEnabled && (
+        <Button className="rounded-full m4-4 p-3 bg-red-200">
+          <Foundation name="telephone" size={20} color="red" />
+        </Button>
+      )}
     </View>
   )
 }
