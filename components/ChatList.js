e534853d1b741e1d587427139e48e4980e8b1eb4
1sh1vam <shivam07.mt@gmail.com>
2023-01-09 20:07:00 +0530

    no matches display

--- components/ChatList.js
+++ components/ChatList.js
@@ -5,11 +5,10 @@ import { db } from '../lib/firebase';
 import useAuth from '../hooks/useAuth';
 
 const ChatList = () => {
-  const [matches, setMatches] = useState();
+  const [matches, setMatches] = useState([]);
   const { user } = useAuth();
 
-  useEffect(() => {
-    onSnapshot(
+  useEffect(() => onSnapshot(
       query(
         collection(db, 'matches'),
         where('userMatched', 'array-contains', user.uid)
@@ -19,13 +18,15 @@ const ChatList = () => {
           snapshot.docs.map((document) => ({ ...document, id: document.id }))
         );
       }
-    );
-  }, [])
+    ), [])
 
-  console.log('matches', matches);
-  return (
+  return matches.length ? (
     <View>
       <Text>ChatList</Text>
+    </View>
+  ) : (
+    <View className="p-5">
+      <Text className="text-center text-lg">No matches so far 🙂</Text>
     </View>
   )
 }
