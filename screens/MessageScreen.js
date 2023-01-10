078dc294cae3392ca0d92b5063da573bc9015e9f
1sh1vam <shivam07.mt@gmail.com>
2023-01-10 20:18:01 +0530

    message screen header

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,21 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, SafeAreaView, StyleSheet, Text } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 import { getMatchedUserInfo } from '../lib/getMatchedUserInfo';
 import useAuth from '../hooks/useAuth';
+import Header from '../components/Header';
 
 const MessageScreen = () => {
   const { params } = useRoute();
   const { user } = useAuth();
   const { matchDetails } = params;
 
-  console.log('math', matchDetails);
   const matchedUser = getMatchedUserInfo(matchDetails.users, user.uid);
-  console.log('user matched', matchedUser);
+
   return (
-    <View>
-      <Text>MessageScreen</Text>
-    </View>
+    <SafeAreaView className="pt-8">
+      <Header title={matchedUser.displayName} callEnabled />
+    </SafeAreaView>
   )
 }
 
