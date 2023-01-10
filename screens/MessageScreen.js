3353e4fb2ecbec20bda78e08ad4925d3fc7c930f
1sh1vam <shivam07.mt@gmail.com>
2023-01-10 20:11:52 +0530

    getting the matched user details

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,7 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
+import { getMatchedUserInfo } from '../lib/getMatchedUserInfo';
+import useAuth from '../hooks/useAuth';
 
 const MessageScreen = () => {
+  const { params } = useRoute();
+  const { user } = useAuth();
+  const { matchDetails } = params;
+
+  console.log('math', matchDetails);
+  const matchedUser = getMatchedUserInfo(matchDetails.users, user.uid);
+  console.log('user matched', matchedUser);
   return (
     <View>
       <Text>MessageScreen</Text>
