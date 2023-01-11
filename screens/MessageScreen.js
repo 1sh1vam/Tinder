38f2ef2ba1b3bc128d96b3b321c7f524c7f850bf
1sh1vam <shivam07.mt@gmail.com>
2023-01-11 20:59:12 +0530

    adding the message to firestore doc

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -7,12 +7,14 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { getMatchedUserInfo } from '../lib/getMatchedUserInfo';
 import useAuth from '../hooks/useAuth';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
+import { db } from '../lib/firebase';
 
 const MessageScreen = () => {
   const { params } = useRoute();
@@ -20,10 +22,18 @@ const MessageScreen = () => {
   const { matchDetails } = params;
 
   const [input, setInput] = useState('');
-
   const matchedUser = getMatchedUserInfo(matchDetails.users, user.uid);
 
+
+
   const handleSendMessage = () => {
+    if (!input) return;
+    addDoc(collection(db, 'matches', matchDetails.id, 'messages'), {
+      uId: user.uid,
+      message: input,
+      photoUrl: user.photoURL,
+      createdAt: serverTimestamp(),
+    });
     setInput('');
   }
 
@@ -33,6 +43,8 @@ const MessageScreen = () => {
       <KeyboardAvoidingView className="flex-1" keyboardVerticalOffset={10}>
         <FlatList
           className="flex-1"
+          data={messages}
+          keyExtractor={(item) => item.id}
         />
         <View className="flex-row bg-white justify-between items-center border border-gray-200 px-5 py-2">
           <TextInput
@@ -40,7 +52,7 @@ const MessageScreen = () => {
             className="text-lg h-10 w-9/12"
             value={input}
             onSubmitEditing={handleSendMessage}
-            onChange={setInput}
+            onChangeText={setInput}
           />
           <Button onPress={handleSendMessage}>
             <Text className="text-red-500 text-lg">Send</Text>
