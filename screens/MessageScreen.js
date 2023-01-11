7dc87f825851b76f67c9f2fc22f96f428bb7c6df
1sh1vam <shivam07.mt@gmail.com>
2023-01-11 20:20:52 +0530

    text input and button for sending text

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,24 +1,46 @@
-import { Platform, SafeAreaView, StyleSheet, Text } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native'
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { useRoute } from '@react-navigation/native';
 import { getMatchedUserInfo } from '../lib/getMatchedUserInfo';
 import useAuth from '../hooks/useAuth';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 const MessageScreen = () => {
   const { params } = useRoute();
   const { user } = useAuth();
   const { matchDetails } = params;
 
+  const [input, setInput] = useState('');
+
   const matchedUser = getMatchedUserInfo(matchDetails.users, user.uid);
 
   return (
-    <SafeAreaView className="pt-8">
+    <SafeAreaView className="pt-8 flex-1">
       <Header title={matchedUser.displayName} callEnabled />
+      <View className="flex-row bg-white justify-between items-center border border-gray-200 px-5 py-2">
+        <TextInput
+          placeholder="Send Message..."
+          className="text-lg h-10 w-9/12"
+          value={input}
+          onChange={setInput}
+        />
+        <Button>
+          <Text className="text-red-500 text-lg">Send</Text>
+        </Button>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default MessageScreen
+export default MessageScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
